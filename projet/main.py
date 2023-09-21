2150cee6e64cc07c1869ea8a0de08587ef85564b
rjezra <jahoely@gmail.com>
2023-09-21 07:01:31 +0300

    ajout condition if

--- projet/main.py
+++ projet/main.py
@@ -1,6 +1,6 @@
-ages=19
-if ages>18 :
-    print("Lehibe")
+heure = int(input("Entre votre heure:"))
+if (heure < 8 or heure > 11) and (heure < 13 or heure > 17):
+    print("le Mangasin est fermer")
 else:
-    print("Ankizy")
+    print("Le mangasin est ouvert")
 
